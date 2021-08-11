1e14aa3d79ba5e3406e01ae09812dbb3a27c9628
enaburekhan <ericenaburekhan2018@gmail.com>
2021-08-11 11:06:02 +0100

    Refactor usersController to use user.save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    if user.valid?
-      user.save
+    if user.save
       payload = { user_id: user.id }
       token = encode_token(payload)
       render json: { username: user.username, user_id: user.id, jwt: token }, status: :created
