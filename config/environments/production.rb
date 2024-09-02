037205884320d8c8b20968309f3a7d982b96f903
enaburekhan <ericenaburekhan2018@gmail.com>
2024-09-02 06:37:06 +0100

    Update environment production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -85,4 +85,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << "https://shrouded-basin-78103-5725827dde51.herokuapp.com/"
+
 end
