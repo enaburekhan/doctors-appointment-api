8348e5ef7b918c7c0a41951d8dd750ed5fdfa335
enaburekhan <ericenaburekhan2018@gmail.com>
2024-09-02 22:03:22 +0100

    Fix authorization error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "https://shrouded-basin-78103-5725827dde51.herokuapp.com/"
+  config.hosts << "shrouded-basin-78103-5725827dde51.herokuapp.com"
 
 end
